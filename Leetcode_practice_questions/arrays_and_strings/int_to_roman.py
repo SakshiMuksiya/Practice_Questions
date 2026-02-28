987cfa83359d08b83727f971ba200c43b334224d
Sakshi Muksiya <sakshimuksiya@gmail.com>
2026-02-28 10:18:23 +0530

    Add files via upload

---
+++ Leetcode_practice_questions/arrays_and_strings/int_to_roman.py
@@ -0,0 +1,19 @@
+#https://leetcode.com/problems/integer-to-roman/submissions/1933276120/?envType=study-plan-v2&envId=top-interview-150
+
+#Solution
+def intToRoman(num: int) -> str:
+    translate={
+        1:"I",
+        5:'V', 4:'IV',
+        10:'X', 9:'IX',
+        50:'L', 40:'XL',
+        100:'C', 90:'XC',
+        500:'D', 400:'CD',
+        1000:'M', 900:'CM'
+    }
+    r= ''
+    for n in [1000,900,500,400,100,90,50,40,10,9,5,4,1]:
+        while n<=num:
+            r+=translate[n]
+            num-=n
+    return r
